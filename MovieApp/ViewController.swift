9708e085ad1a6293c47cc11d008c029481d4b81f
Tong Viet Dung <tongvietdung@admins-Mac-mini-4.local>
2024-11-05 14:54:20 +0700

    Initial Commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Tong Viet Dung on 5/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
